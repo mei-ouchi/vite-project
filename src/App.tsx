a99f14975ea0de3149f808338d53e64948fc9f78
EXCITE-2108-02\mei ouchi <mei.ouchi@excite-software.net>
2025-08-19 17:52:33 +0900

    クイックスタート：リストのレンダー

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
 
-function MyButton(){
-  return(
-    <button>I'm button</button>
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
   );
-}
 
-export default function MyApp(){
-  return(
-    <div>
-    <h1>Welcome to Iwadeyama!</h1>
-    <MyButton />
-    </div>
+  return (
+    <ul>{listItems}</ul>
   );
 }
